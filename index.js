67043b08eee5611cd53b4641ba61355cf6aeb791
ekodhani <ekonurramadhani@gmail.com>
2021-02-12 04:43:02 +0700

    Update Index Express Router

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 
-app.use(() => {
-    console.log('hello dhan!')
+router.use('/products', (req, res, next) => {
+    res.json({ name: "dhani", email: "ekonurramadhani@gmail.com" });
+    next();
 })
+
+router.use('/price', (req, res, next) => {
+    res.json({ price: 30000000 });
+    next();
+})
+
+router.delete('/customers', (req, res, next) => {
+    res.json({ title: "Customer" });
+    next();
+})
+
+
+app.use('/', router);
 
 app.listen(4000);
